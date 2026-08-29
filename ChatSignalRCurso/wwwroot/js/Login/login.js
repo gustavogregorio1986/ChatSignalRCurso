f45f7649ba2a052ff2f901630c12daa0e40413e4
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-08-29 13:59:29 -0300

    Desenvolvimento da entrada no chat

---
+++ ChatSignalRCurso/wwwroot/js/Login/login.js
@@ -0,0 +1,14 @@
+﻿document.getElementById('entrarSala').onclick = function () {
+    const usuario = document.getElementById('usuario').value;
+    const nomeSala = document.getElementById('nomeSala').value;
+
+    if (!usuario || !nomeSala) {
+        alert("Preencha usuário e sala!");
+        return;
+    }
+
+    sessionStorage.setItem("usuario", usuario);
+    sessionStorage.setItem("nomeSala", nomeSala);
+
+    window.location.href = "/Home/Index";
+};
