4879d0790f2215aa3b94e50e350366bfec7d6b6c
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-08-29 13:18:47 -0300

    configiurar o hub paar signalR que é opvcional, antes era automatico e agora esta precisando no projeto

---
+++ ChatSignalRCurso/Hubs/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalRCurso.Hubs
+{
+    public class ChatHub : Hub
+    {
+    }
+}
--- ChatSignalRCurso/Program.cs
+++ ChatSignalRCurso/Program.cs
@@ -1,4 +1,5 @@
 using ChatSignalRCurso.Data;
+using ChatSignalRCurso.Hubs;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -29,5 +32,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Login}/{id?}")
     .WithStaticAssets();
 
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
