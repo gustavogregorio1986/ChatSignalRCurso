42c4b8eed716791de048c7d028cc14bdadef90b2
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-08-29 19:25:26 -0300

    Desenvolvimento do metodo Hub no projeto

--- ChatSignalRCurso/Hubs/ChatHub.cs
+++ ChatSignalRCurso/Hubs/ChatHub.cs
@@ -4,5 +4,24 @@ namespace ChatSignalRCurso.Hubs
 {
     public class ChatHub : Hub
     {
+        public static readonly Dictionary<string, Dictionary<string, string>> Salas = new();
+
+        public async Task EntrarSala(string sala, string usuario)
+        {
+
+            if (!Salas.ContainsKey(sala))
+            {
+                Salas[sala] = new Dictionary<string, string>();
+            }
+
+            Salas[sala][Context.ConnectionId] = usuario;
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, sala);
+            await Clients.OthersInGroup(sala).SendAsync("MensagemSistema", $"{usuario} entrou na sala");
+
+
+        }
+
+
     }
 }
